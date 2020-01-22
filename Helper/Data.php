e5bb8adcffc679b09da823d604bd175bdd99e42d
Amin Mehrzad <amin.mehrzad@gmail.com>
2020-01-22 09:23:34 -0800

    Observer POST order information to server

--- Helper/Data.php
+++ Helper/Data.php
@@ -9,6 +9,8 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
 
     const XML_PATH_RATING_APP_USERNAME = 'ratingapp_tab/ratingapp_setting/ratingapp_username';
     const XML_PATH_RATING_APP_PASSWORD = 'ratingapp_tab/ratingapp_setting/ratingapp_password';
+    const XML_PATH_RATING_APP_TOKEN = 'ratingapp_tab/ratingapp_setting/token';
+    const XML_PATH_RATING_APP_REFRESH_TOKEN = 'ratingapp_tab/ratingapp_setting/refresh_token';
 
     public function __construct(
         \Magento\Framework\App\Helper\Context $context,
@@ -24,34 +26,47 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     {
         return $this->_scopeConfig->getValue(self::XML_PATH_RATING_APP_USERNAME);
     }
+    
     public function ratingApp_password()
     {
         return $this->_scopeConfig->getValue(self::XML_PATH_RATING_APP_PASSWORD);
     }
 
+    public function ratingApp_token()
+    {
+        return $this->_scopeConfig->getValue(self::XML_PATH_RATING_APP_TOKEN);
+    }
+
     public function ratingApp_refreshToken()
     {
-        $username = $this->ratingApp_username();
-        $password = $this->ratingApp_password();
-
-        $url = 'http://amin.ngrok.io/API/users/authenticate';
-
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
-            'name' => 'Amin3',
-            'email' => 'amin@amin.com',
-            'password' => '123456')
-        ));
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        $response = curl_exec($ch);
-        curl_close($ch);
-        error_log($response);
-        $result = json_decode($response, true);
-       // $refreshToken = trim(json_encode($result['data']['refreshToken'], true),'"');
-
-       // return $refreshToken;
-        return $result;
+        return $this->_scopeConfig->getValue(self::XML_PATH_RATING_APP_REFRESH_TOKEN);
     }
+
+
+
+    // public function ratingApp_refreshToken()
+    // {
+    //     $username = $this->ratingApp_username();
+    //     $password = $this->ratingApp_password();
+
+    //     $url = 'http://amin.ngrok.io/API/users/authenticate';
+
+    //     $ch = curl_init($url);
+    //     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
+    //         'name' => 'Amin3',
+    //         'email' => 'amin@amin.com',
+    //         'password' => '123456')
+    //     ));
+    //     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    //     $response = curl_exec($ch);
+    //     curl_close($ch);
+    //     error_log($response);
+    //     $result = json_decode($response, true);
+    //     $refreshToken = trim(json_encode($result['data']['refreshToken'], true),'"');
+
+    //     return $refreshToken;
+    //     // return $response;
+    // }
 }
--- Observer/ChangeState.php
+++ Observer/ChangeState.php
@@ -2,11 +2,11 @@
 
 namespace RatingApp\Rate\Observer;
 
-use Magento\Framework\Event\ObserverInterface;
-use Magento\Framework\App\Request\DataPersistorInterface;
 use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Event\ObserverInterface;
 use Magento\Sales\Api\OrderRepositoryInterface;
 use RatingApp\Rate\Helper\Data;
+
 //use RatingApp\Rate\Api\ApiManagementInterface;
 //use \Magento\Framework\Stdlib\CookieManagerInterface;
 
@@ -18,43 +18,48 @@ class ChangeState implements ObserverInterface
     protected $_order;
     protected $_state;
     protected $helper;
-   // protected $_cookieManager;
+    // protected $_cookieManager;
     protected $orderRepository;
     protected $orderManagement;
     protected $_checkoutSession;
     protected $_session_data;
+    protected $imageHelper;
+    protected $productRepository;
 
-
-public function __construct(
-    \Magento\Sales\Api\OrderManagementInterface $orderManagement,
-   // ApiManagementInterface $apiManagement,
-    \Magento\Backend\Model\Session\Quote $sessionQuote,
-    \Magento\Sales\Api\Data\OrderInterface $order,
-    \Magento\Directory\Model\CountryFactory $countryFactory,
-    \Magento\Framework\App\State $state,
-  //  CookieManagerInterface $cookieManager,
-    OrderRepositoryInterface $orderRepository,
-    Data $helper,
-    \Magento\Checkout\Model\Session $checkoutSession,
-    \Magento\Quote\Model\QuoteFactory $quoteFactory
-
+    public function __construct(
+        \Magento\Store\Model\StoreManagerInterface $storeManagerInterface,
+        \Magento\Catalog\Helper\Image $imageHelper,
+        \Magento\Catalog\Api\ProductRepositoryInterface $productRepository,
+        \Magento\Sales\Api\OrderManagementInterface $orderManagement,
+        // ApiManagementInterface $apiManagement,
+        \Magento\Backend\Model\Session\Quote $sessionQuote,
+        \Magento\Sales\Api\Data\OrderInterface $order,
+        \Magento\Directory\Model\CountryFactory $countryFactory,
+        \Magento\Framework\App\State $state,
+        //  CookieManagerInterface $cookieManager,
+        OrderRepositoryInterface $orderRepository,
+        Data $helper,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Quote\Model\QuoteFactory $quoteFactory
 
     ) {
 
-       // $this->_apiManagement = $apiManagement;
+        // $this->_apiManagement = $apiManagement;
         $this->_order = $order;
         $this->_sessionQuote = $sessionQuote;
         $this->_countryFactory = $countryFactory;
         $this->_state = $state;
         $this->helper = $helper;
-       //$this->_cookieManager = $cookieManager;
+        //$this->_cookieManager = $cookieManager;
         $this->orderRepository = $orderRepository;
         $this->quoteFactory = $quoteFactory;
         $this->orderManagement = $orderManagement;
         $this->_checkoutSession = $checkoutSession;
         $this->_session_data = [];
+        $this->imageHelper = $imageHelper;
+        $this->productRepository = $productRepository;
+        $this->_storeManager = $storeManagerInterface;
     }
-
 
     // private function _addFieldToOrderData($strFieldName, $strValue)
     // {
@@ -71,100 +76,119 @@ public function __construct(
      */
     private function _getEmailData($order)
     {
- 
-            $this->_email_data=array(
-                "profile"   =>[
-                    "firstname"         =>"",
-                    "lastname"          =>"",
-                    "street"            =>[
-                                            "0" =>"",
-                                            "1" =>"",
-                                            "2" =>""
-                    ],
-                    "city"              =>"",
-                    "region"            =>"",
-                    "region_id"         =>"",
-                    "postcode"          =>"",
-                    "country"           =>"",
-                    "country_id"        =>"",
-                    "dob"               =>"",
-                    "ssn"               =>""
-                ],
-                "person"    =>[
-                    "email"             =>"",
-                    "telephone"         =>"",
-                    "password"          =>"",
-                    "confirmPassword"   =>"",
-                    "confirmationCode"  =>""         
-                ],
-                "token"     =>"",
-                "session_id"=>"",
-               "order"     =>[
-                    "order_data"        =>"",
-                    "order_number"      =>$order->getIncrementId(),
-                    "order_total"       =>"",
-                    "order_status"      =>$order->getStatus(),
-                    "order_date"        =>""
-               ]
-            );
+
+        $orderItems = $order->getAllItems();
+
+        foreach ($orderItems as $item) {
+            error_log(print_r($item->getData(), true));
+            $itemSKUs[] = $item->getSku();
+            $itemsNames[] = $item->getName();
+            $itemsIds[] = $item->getProductId();
+            try {
+                $_product = $this->productRepository->get($item->getSku());
+                //$image_url = $this->imageHelper->init($_product, 'product_page_image_large')->setImageFile($_product->getFile())->getUrl();
+                // $imageURLs[]=$image_url;
+                $image_url = $_product->getData('image');
+                $imageURLs[] = $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . 'catalog/product' . $image_url;
+
+                //error_log(print_r($img, true));
+
+            } catch (NoSuchEntityException $e) {
+                return 'product not found';
+            }
+
+            // echo $item->getProductId();
+            // print_r($item->getData());
+        }
+        // error_log(print_r($itemSKUs[0], true));
+       //  error_log(print_r($order->getBillingAddress()->getData(), true));
+
+        $this->_email_data = array(
+            "productUID" => $itemsIds[0],
+            "productName" => $itemsNames[0],
+            "productImage" => $imageURLs[0],
+            "email" => $order->getBillingAddress()->getEmail(),
+            "firstName" => $order->getBillingAddress()->getFirstname(),
+            "lastName" => $order->getBillingAddress()->getLastname(),
+            //"status" => "",
+            //"createdTime" => $order->getCreatedAt(),
+            "websiteID" => $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_WEB),
+            // "emailUID" => time().'-'.rand()
+        );
+
+        error_log(print_r($this->_email_data, true));
 
     }
 
-
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
+        error_log('>>>>>>>>>>>>>>>>>>>================---------->>>>>>>>>>>>>>>>>>>>>>>>>');
+        $orderData = $observer->getEvent()->getOrder()->getData();
 
+        $items = $observer->getEvent()->getOrder()->getItems();
 
-    $orderData = $observer->getEvent()->getOrder()->getData();
+        $orderId = $orderData['increment_id'];
+        $quoteId = $orderData['quote_id'];
+        error_log(print_r($orderId, true));
 
-    $items= $observer->getEvent()->getOrder()->getItems();
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $order = $objectManager->create('Magento\Sales\Model\Order')->loadByIncrementId($orderId);
 
-    $orderId = $orderData['increment_id'];
-    $quoteId = $orderData['quote_id'];
+        // error_log(print_r($order->getBillingAddress()->getFirstname(),true));
 
-    $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-    $order = $objectManager->create('Magento\Sales\Model\Order')->loadByIncrementId($orderId);
+        $entityId = $order->getEntityId();
 
+        $orderData2 = $order->getData();
 
-    $entityId=$order->getEntityId();
+        $orderState = $order->getState();
 
-    $orderData2=$order->getData();
+        $orderTotalItems = $orderData['total_item_count'];
 
-    $orderItems = $order->getAllItems();
+        $orderDate = $order->getCreatedAt();
 
-    $orderState = $order->getState();
+        $orderUpdate = $order->getUpdatedAt();
 
-    $orderTotalItems = $orderData['total_item_count'];
+        //$avcCookie = $this->_cookieManager->getCookie('aspire_token');
+        //$session_id = $this->_cookieManager->getCookie('session_id');
 
-    $orderDate = $order->getCreatedAt();
+        //  $cyaOrderHold = $this->_checkoutSession->getCyaOrderHold();
 
-    $orderUpdate = $order->getUpdatedAt();
+        $this->_getEmailData($order);
 
-    //$avcCookie = $this->_cookieManager->getCookie('aspire_token');
-    //$session_id = $this->_cookieManager->getCookie('session_id');
+        // $tokens = $this->helper->ratingApp_refreshToken();
+        // $decodedTokens = json_decode($tokens, true);
+        // $appRefreshToken = $decodedTokens['data']['refreshToken'];
+        // $appToken = $decodedTokens['data']['token'];
 
-    $cyaOrderHold = $this->_checkoutSession->getCyaOrderHold();
+        $appToken = $this->helper->ratingApp_token();
 
-    $this->_getEmailData($order);
+        $url = 'http://amin.ngrok.io/API/emails';
 
-    $key = $this->helper->ratingApp_apiKey();
-    $secretKey = $this->helper->ratingApp_secretKey();
-    $session_data["website_key"]= $key;
-         $url = 'http://amin.ngrok.io/emails';
-        
-        $authorization = "Authorization: Bearer ".$secretKey;
+        $authorization = "Authorization: Bearer " . $appToken;
 
         $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization ));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', $authorization));
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($this->_email_data));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($this->_email_data));
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         $result = curl_exec($ch);
-        curl_close($ch);
-   // $res = $this->_apiManagement->change_state($this->_session_data);
 
-    error_log($result);
+        // Check HTTP status code
+        if (!curl_errno($ch)) {
+            switch ($http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE)) {
+                case 200: # OK
+                    break;
+                default:
+                    error_log('Unexpected HTTP code: ' . $http_code);
+            }
+        }
+
+        curl_close($ch);
+
+        // $res = $this->_apiManagement->change_state($this->_session_data);
+
+        error_log($result);
 
     }
 }
